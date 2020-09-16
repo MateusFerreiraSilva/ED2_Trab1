edb7ff29b01421091a6e2ed861aebb860eace307
Mateus Ferreira Silva <54829121+MateusFerreiraSilva@users.noreply.github.com>
2020-09-16 00:26:33 -0300

    Add files via upload

--- trash_table_sort.cpp
+++ trash_table_sort.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 void print_vector(vector<auto> &v) {
-	cout << endl;
 	for(auto a : v) cout << a << endl;
 } // print_vector
 
@@ -14,27 +13,26 @@ int disorder_degree(vector<auto> &v, int n) {
 	return d;
 } // disorder_degree
 
-void inteiros(string file) {
+void int_sort(string file) {
 	vector<int> v;
 	int in, n;
 	ifstream input(file);
 
-	if(input.fail()) {
-		cout << "Arquivo não encontrado\n";
-		return;
+	if(input.is_open()) {
+		while(input >> in) {
+			if(input.good()) 
+				v.push_back(in);
+		} // while
 	} // if
-
-	while(input >> in)
-		v.push_back(in);
 
 	n = v.size();
 
 	vector<vector<int>> s;
 	s.reserve(n);
-	// O(n³)
+	// O(n³) -> n()
 	for(int i = 0; i < n; i++) { // n
 
-		// O(n²) = n(n-1)
+		// O(n²) -> n(n-1) = n²-n
 		for(int j = 0; j < n; j++) { // n
 
 			int pivot = v[j];
@@ -59,13 +57,6 @@ void inteiros(string file) {
 
 			for(int k = 0; k < right.size(); k++) 
 				s[j].push_back(right[k]);
-
-			//print
-			// for(int k = 0; k < n; k++) {
-			// 		cout << s[j][k];
-			// 		if(k == n-1) cout << endl;
-			// 		else cout << " ";
-			// } // for
 		} // for
 
 		int vd = disorder_degree(v, n); // catch the disorer degree of the vector of input
@@ -123,123 +114,18 @@ void inteiros(string file) {
 	} // for
 } // inteiros
 
-void frases(string file) {
+void str_sort(string file) {
 	vector<string> v;
 	int n;
 	string in;
 	ifstream input(file);
 
-	if(input.fail()) {
-		cout << "Arquivo não encontrado\n";
-		return;
+	if(input.is_open()) {
+		while(getline(input, in)) {
+			if(input.good()) 
+				v.push_back(in);
+		} // while
 	} // if
-
-	while(getline(input, in))
-		v.push_back(in);
-
-	n = v.size();
-
-	vector<vector<string>> s;
-	s.reserve(n);
-	// O(n³)
-	for(int i = 0; i < n; i++) { // n
-
-		// O(n²) = n(n-1)
-		for(int j = 0; j < n; j++) { // n
-
-			string pivot = v[j];
-			vector<string> left, right, mid;
-
-			for(int k = 0; k < n; k++) { // n-1 
-				if(v[k] > pivot){
-					right.push_back(v[k]);
-				}
-				else if(v[k] < pivot){
-					left.push_back(v[k]);
-				}
-				else
-					mid.push_back(v[k]);
-			} // for
-
-			for(int k = 0; k < left.size(); k++) 
-				s[j].push_back(left[k]);
-
-			for(int k = 0; k < mid.size(); k++)
-				s[j].push_back(mid[k]);
-
-			for(int k = 0; k < right.size(); k++) 
-				s[j].push_back(right[k]);
-		} // for
-
-		int vd = disorder_degree(v, n); // catch the disorer degree of the vector of input
-
-		vector<int> d;
-		d.reserve(n);
-		for(int j = 0; j < n; j++) {
-			d[j] = disorder_degree(s[j], n); // catch the disorder degree of each line on the matrix
-			if(d[j] == 0) { // we find a sorted array
-				print_vector(s[j]);
-				return;
-			} // if
-		} // for
-
-		int min;
-		list<int> index; // store the index of lesser disorder degree in a pseudo sorted order
-		for(int j = 0; j < n; j++) {
-			if(j == 0 || d[j] <= min) {
-				min = d[j];
-				index.push_front(j);
-			} else
-				index.push_back(j);
-		} // for
-
-		int idx_cp;
-		for(int idx : index) {
-			if(d[idx] == 0) { // we find a sorted array
-				print_vector(s[idx]);
-				return;
-			} else if(d[idx] == vd) { // we have to check if this array is different of the original one 
-				bool flag = false;
-				for(int j = 0; j < n; j++) {
-					if(s[idx][j] != v[j]) {
-						flag = true;
-						break;
-					} // if
-				} // for
-				if(flag) {
-					idx_cp = idx;
-					break;
-				} // if
-
-			} else {
-				idx_cp = idx;
-				break;
-			} // else
-		} // for
-
-		// reset
-		for(int j = 0; j < n; j++)
-			v[j] = s[idx_cp][j];
-		for(int j = 0; j < n; j++)
-				s[j].clear();
-
-	} // for
-} // frases
-
-
-void dna(string file) {
-	vector<string> v;
-	int n;
-	string in;
-	ifstream input(file);
-
-	if(input.fail()) {
-		cout << "Arquivo não encontrado\n";
-		return;
-	} // if
-
-	while(getline(input, in))
-		v.push_back(in);
 
 	n = v.size();
 
@@ -332,19 +218,31 @@ void dna(string file) {
 	} // for
 } // dna
 
-int main(int argc, char **argv) {
-	if(argc != 3)
-		cout << "Entrada inválida\n";
+int main() {
+	string file = "data.txt";
+	ifstream input(file);
+	string in;
+	
+	if(input.is_open())
+		getline(input, in);
+
 	else {
-
-		string str1 = argv[1];
-		string str2 = argv[2];
-
-		if(str1 == "inteiros") inteiros(str2);
-		else if(str1 == "frases") frases(str2);
-		else if(str1 == "dna") dna(str2);
-		else cout << "Entrada inválida\n";
+		cout << "Arquivo não encontrado\n";
+		return 0;
 	} // else
 
+	// check type of input
+	int digit_cont = 0;
+	for(int i = 0; i < in.size(); i++) {
+		if(isdigit((char) in[i]))
+			digit_cont++;
+		else break;
+	} // for
+
+	if(digit_cont == in.size())
+		int_sort(file);
+	else 
+		str_sort(file);
+
 	return 0;
-}
+} // main
